0970794dff7ed5ec285c78225538092e87f00694
ranroun3 <rony.stephan@gmail.com>
2021-07-23 12:41:52 -0400

    Add files via upload

---
+++ Lab6/SavingsAccount.java
@@ -0,0 +1,28 @@
+
+public class SavingsAccount extends BankAccount {
+	private double rate = 0.025;
+	private int savingsNumber=0;
+	private String accountNumber;
+	public SavingsAccount(String name, double initialBalance) {
+		super(name, initialBalance);
+		accountNumber=super.getAccountNumber() + "-" + savingsNumber;
+	}
+	public void postInterest() {
+		double interest = rate*super.getBalance();
+		super.deposit(interest);
+	}
+	@Override
+	public String getAccountNumber()
+	{
+		return this.accountNumber;
+	}
+	public int getSavingsNumber() {
+		return savingsNumber;
+	}
+	public SavingsAccount(SavingsAccount oldAccount, int initialBalance) {
+		super(oldAccount, initialBalance);
+		savingsNumber=oldAccount.getSavingsNumber()+1;
+		accountNumber= oldAccount.getAccountNumber() + "-" + savingsNumber;
+	}
+	
+}
